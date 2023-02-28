74e8de068b3815d20265c203e0c75949bc6af265
Gary Yendell <gary.yendell@diamond.ac.uk>
2023-02-28 00:41:47 +0000

    DNA Encoding the right way

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,11 +1,11 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
     case code_point do
-      c when c in [' ', ?\s] -> 0b0000
-      c when c in ['A', ?A] -> 0b0001
-      c when c in ['C', ?C] -> 0b0010
-      c when c in ['G', ?G] -> 0b0100
-      c when c in ['T', ?T] -> 0b1000
+      ?\s -> 0b0000
+      ?A -> 0b0001
+      ?C -> 0b0010
+      ?G -> 0b0100
+      ?T -> 0b1000
     end
   end
 
@@ -20,32 +20,26 @@ defmodule DNA do
   end
 
   def encode(dna) do
-    do_encode(dna)
+    do_encode(dna, <<>>)
   end
 
-  defp do_encode([nucleotide | tail]) when length(tail) > 0 do
-    <<do_encode(nucleotide)::bitstring, do_encode(tail)::bitstring>>
+  defp do_encode([nucleotide | tail], encoded) do
+    do_encode(tail, <<encoded::bitstring, encode_nucleotide(nucleotide)::4>>)
   end
 
-  defp do_encode(dna) do
-    <<encode_nucleotide(dna)::4>>
+  defp do_encode([], encoded) do
+    encoded
   end
 
   def decode(dna) do
-    do_decode(dna)
+    do_decode(dna, '')
   end
 
-  defp do_decode(<<nucleotide::4, tail::bitstring>>) when tail != "" do
-    do_decode(<<nucleotide::4>>) ++ do_decode(tail)
+  defp do_decode(<<nucleotide::4, tail::bitstring>>, decoded) do
+    do_decode(tail, decoded ++ [decode_nucleotide(nucleotide)])
   end
 
-  defp do_decode(nucleotide) do
-    case nucleotide do
-      <<0b0000::4>> -> ' '
-      <<0b0001::4>> -> 'A'
-      <<0b0010::4>> -> 'C'
-      <<0b0100::4>> -> 'G'
-      <<0b1000::4>> -> 'T'
-    end
+  defp do_decode(<<0::0>>, decoded) do
+    decoded
   end
 end
